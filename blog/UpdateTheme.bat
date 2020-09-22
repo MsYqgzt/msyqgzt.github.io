a339ce25ab9b187004405dbd7d846b1cab283137
MsYqgzt <msyqgzt_nick@163.com>
2020-09-22 18:05:06 +0800

    upload

--- blog/UpdateTheme.bat
+++
@@ -1,1 +0,0 @@
-git clone https://github.com/ChrAlpha/hexo-theme-cards.git themes/cards
---
+++ blog/localRun.ps1
@@ -0,0 +1,5 @@
+# starts with default browser and adds to open browser
+Start-Process -FilePath http://localhost:4000
+
+# run local server
+hexo s
